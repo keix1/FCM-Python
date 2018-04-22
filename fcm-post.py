643086341b86345d615276343268764a7507cd8e
ke_ix1 <xkei4dev@gmail.com>
2018-04-22 18:13:40 +0900

    initial commit

---
+++ fcm-post.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+# Send to single device.
+from pyfcm import FCMNotification
+import settings
+API_KEY = settings.AP
+DEVICE_REGISTRATION_ID = settings.DRI
+
+push_service = FCMNotification(api_key=API_KEY)
+
+# OR initialize with proxies
+
+# proxy_dict = {
+#           "http"  : "http://127.0.0.1",
+#           "https" : "http://127.0.0.1",
+#         }
+# push_service = FCMNotification(api_key="<api-key>", proxy_dict=proxy_dict)
+
+# Your api-key can be gotten from:  https://console.firebase.google.com/project/<project-name>/settings/cloudmessaging
+
+registration_id = DEVICE_REGISTRATION_ID
+message_title = "News!"
+message_body = "OKKKKKKKKKKKKKKKKKK!!!!!!!!"
+result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+
+# Send to multiple devices by passing a list of ids.
+# registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
+# message_title = "Uber update"
+# message_body = "Hope you're having fun this weekend, don't forget to check today's news"
+# result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title, message_body=message_body)
+
+print(result)
