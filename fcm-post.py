be8389a50e60a33d05c679486cdce6f4622c0707
ke_ix1 <xkei4dev@gmail.com>
2018-04-23 20:02:19 +0900

    I confirmed receive notification on wearable

--- fcm-post.py
+++ fcm-post.py
@@ -8,8 +8,16 @@ DEVICE_REGISTRATION_ID = settings.DRI
 push_service = FCMNotification(api_key=API_KEY)
 
 registration_id = DEVICE_REGISTRATION_ID
-message_title = "News!"
-message_body = "OKKKKKKKKKKKKKKKKKK!!!!!!!!"
-result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+data_message = {
+    "title" : "News!!",
+    "body" : "great match!",
+    "data" : "PortugalVSDenmark",
+}
 
+# message_title = "Uber update"
+# message_body = "Hi john, your customized news for today is ready"
+
+result = push_service.single_device_data_message(registration_id=registration_id, data_message=data_message)
+
+# result = push_service.notify_single_device(registration_id=registration_id, data_message=data_message, message_body=message_body)
 print(result)
